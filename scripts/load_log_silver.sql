a81432475fcee9c18d62bab2375be0948705ee08
FFors <f.fors@live.se>
2026-03-05 16:52:51 +0100

    feat: skapa tabell för loggning av proceduren load_silver

---
+++ scripts/load_log_silver.sql
@@ -0,0 +1,16 @@
+/* ---
+Denna koden skapar tabellen load_log i silverschemat som ska matas av data från proceduren load_silver som transformerar och laddar 
+in data i silver.prices.
+*/ ---
+
+IF OBJECT_ID('silver.load_log', 'U') IS NULL
+BEGIN
+	CREATE TABLE silver.load_log (
+	    load_id         INT identity(1,1) primary key,
+        source_file     VARCHAR(260) NULL,
+        rows_inserted   INT NULL,
+        status_type     VARCHAR(20) NULL, /* kan innehålla värden som SUCCESS, FAILED, RETRY, PARTIAL_SUCCESS osv*/
+	    error_message   VARCHAR(500) NULL,
+	    proc_timestamp  DATETIME2 NOT NULL DEFAULT sysdatetime()
+	);
+END;
