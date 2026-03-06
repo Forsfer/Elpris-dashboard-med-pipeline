6af1f02ba87c51a50c0ccbfe2434244a8b0cdee8
FFors <f.fors@live.se>
2026-03-06 11:24:40 +0100

    feat: skapa procedure_name så det spåras vad som laddade in data

--- scripts/load_log_silver.sql
+++ scripts/load_log_silver.sql
@@ -7,6 +7,7 @@ IF OBJECT_ID('silver.load_log', 'U') IS NULL
 BEGIN
 	CREATE TABLE silver.load_log (
 	    load_id         INT identity(1,1) primary key,
+		procedure_name  VARCHAR(100),
         source_file     VARCHAR(260) NULL,
         rows_inserted   INT NULL,
         status_type     VARCHAR(20) NULL, /* kan innehålla värden som SUCCESS, FAILED, RETRY, PARTIAL_SUCCESS osv*/
